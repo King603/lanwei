d09be536937017c93076e0c2304543239e8f4dab
45555 <408385413@qq.com>
2020-09-22 17:55:51 +0800

    修改已发现的错误显示 封装部分交互信息数据

--- lw/src/pages/user/pwd/pwd.vue
+++ lw/src/pages/user/pwd/pwd.vue
@@ -1,9 +1,24 @@
 <template>
   <view class="content">
     <view class="input-group">
-      <view class="input-row">
-        <text class="title">邮箱：</text>
-        <m-input type="text" focus clearable v-model="email" placeholder="请输入邮箱"></m-input>
+      <view class="input-row border">
+        <text class="title">账号：</text>
+        <m-input type="text" focus clearable v-model="userName" placeholder="请输入原始账号"></m-input>
+      </view>
+
+      <view class="input-row border">
+        <text class="title">绑定的手机号：</text>
+        <m-input type="text" focus clearable v-model="phone" placeholder="请输入手机号"></m-input>
+      </view>
+
+      <view class="input-row border">
+        <text class="title">密码：</text>
+        <m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
+      </view>
+
+      <view class="input-row border">
+        <text class="title">确认密码：</text>
+        <m-input type="password" displayable v-model="confirmPassword" placeholder="请确认密码"></m-input>
       </view>
     </view>
 
@@ -14,8 +29,54 @@
 </template>
 
 <script>
-import vue from "./pwd.js";
-export default vue;
+// import vue from "./pwd.js";
+// export default vue;
+
+import mInput from "../../../components/m-input.vue";
+
+export default {
+  components: {
+    mInput,
+  },
+  data() {
+    return {
+      userName: "",
+      phone: "",
+    };
+  },
+  methods: {
+    findPassword() {
+      if (this.userName.length < 3) {
+        uni.showToast({
+          icon: "none",
+          title: "账号最短为 3 个字符",
+        });
+        return;
+      }
+      if (!/^1[3456789]\d{9}$/.test(this.phone)) {
+        uni.showToast({
+          icon: "none",
+          title: "手机号码填写错误",
+        });
+        return;
+      }
+      if (this.password.length < 6) {
+        uni.showToast({
+          icon: "none",
+          title: "密码最短为 6 个字符",
+        });
+        return;
+      }
+      if (this.password !== this.confirmPassword) {
+        uni.showToast({
+          icon: "none",
+          title: "两次密码输入不一致",
+        });
+        return;
+      }
+    },
+  },
+};
 </script>
 
 <style>
--- lw/src/pages/user/reg/reg.vue
+++ lw/src/pages/user/reg/reg.vue
@@ -1,17 +1,22 @@
 <template>
   <view class="content">
+    <text class="prompt">*号为设置后不可更改项</text>
     <view class="input-group">
       <view class="input-row border">
-        <text class="title">账号：</text>
+        <text class="title red">账号：</text>
         <m-input type="text" focus clearable v-model="username" placeholder="请输入账号"></m-input>
       </view>
       <view class="input-row border">
         <text class="title">密码：</text>
         <m-input type="password" displayable v-model="password" placeholder="请输入密码"></m-input>
       </view>
-      <view class="input-row">
+      <view class="input-row border">
         <text class="title">确认密码：</text>
         <m-input type="password" displayable v-model="confirmPassword" placeholder="请确认密码"></m-input>
+      </view>
+      <view class="input-row border">
+        <text class="title red">绑定电话：</text>
+        <m-input type="phone" displayable v-model="PhoneNum" placeholder="请输入手机号码"></m-input>
       </view>
     </view>
     <view class="btn-row">
@@ -21,8 +26,95 @@
 </template>
 
 <script>
-import vue from "./reg.js";
-export default vue;
+import mInput from "../../../components/m-input.vue";
+
+export default {
+  components: {
+    mInput,
+  },
+  data() {
+    return {
+      username: "",
+      password: "",
+      confirmPassword: "",
+      PhoneNum: "",
+    };
+  },
+  methods: {
+    register() {
+      /**
+       * 客户端对账号信息进行一些必要的校验。
+       * 实际开发中，根据业务需要进行处理，这里仅做示例。
+       */
+      if (this.username.length < 3) {
+        uni.showToast({
+          icon: "none",
+          title: "账号最短为 3 个字符",
+        });
+        return;
+      }
+      if (this.password.length < 6) {
+        uni.showToast({
+          icon: "none",
+          title: "密码最短为 6 个字符",
+        });
+        return;
+      }
+      if (this.password !== this.confirmPassword) {
+        uni.showToast({
+          icon: "none",
+          title: "两次密码输入不一致",
+        });
+        return;
+      }
+      if (!/^1[3456789]\d{9}$/.test(this.PhoneNum)) {
+        uni.showToast({
+					icon:"none",
+					title:"电话号码输入错误"
+				});
+        return;
+      }
+
+      const params = {
+        userName: this.username,
+        passWord: this.password,
+        phoneNum: this.phoneNum,
+      };
+      uniCloud.callFunction({
+        name: "user-center",
+        data: {
+          action: "register",
+          params: params,
+        },
+        success(e) {
+          console.log("注册成功", e);
+
+          if (e.result.code === 0) {
+            uni.showToast({
+              title: "注册成功",
+            });
+            uni.setStorageSync("uniIdToken", e.result.token);
+            uni.setStorageSync("username", e.result.username);
+            uni.reLaunch({
+              url: "../main/main",
+            });
+          } else {
+            uni.showModal({
+              content: JSON.stringify(e.result),
+              showCancel: false,
+            });
+          }
+        },
+        fail(e) {
+          uni.showModal({
+            content: JSON.stringify(e),
+            showCancel: false,
+          });
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style>
